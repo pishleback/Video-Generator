2da9f5003b56417fce55c19826a84be7198376a9
Michael <michael@otopia.co.uk>
2026-05-10 16:35:21 +0100

    Update example.rs

--- animator/src/bin/example.rs
+++ animator/src/bin/example.rs
@@ -1,5 +1,5 @@
 use animator::{
-    colour::{Colour, ColourWithAlpha},
+    colour::{Colour, ColourBuilder, ColourWithAlpha},
     slideshow::SlideshowBuilder,
 };
 use std::path::Path;
@@ -7,6 +7,8 @@ use std::path::Path;
 fn main() {
     let mut slideshow =
         SlideshowBuilder::<1920, 1080>::new(ColourWithAlpha::from_srgb(0.0, 0.0, 0.0, 1.0));
+
+    slideshow.slide(|slide| {});
 
     slideshow
         .slide(|slide| {
@@ -114,6 +116,41 @@ fn main() {
             });
         })
         .duration(2.0);
+
+    slideshow
+        .slide(|slide| {
+            slide.canvas(|canvas, t| {
+                let c = (0.2 * t) % 1.0;
+                for x in 0..20 {
+                    for y in 0..20 {
+                        let x = x as f64;
+                        let y = y as f64;
+
+                        let a = x / 20.0;
+                        let b = y / 20.0;
+
+                        canvas.circle((x, y), 0.5).fill_rgba(
+                            ColourBuilder::new()
+                                .hue_rad(c * std::f64::consts::TAU)
+                                .lightness(a)
+                                .saturation(b)
+                                .finish()
+                                .alpha(1.0),
+                        );
+                    }
+                }
+
+                canvas
+                    .latex(format!(
+                        "\\texttt{{{:.0} {:.1}}}",
+                        c * 360.0,
+                        c * std::f64::consts::TAU
+                    ))
+                    .position((10.0, -2.0))
+                    .height(2.0);
+            });
+        })
+        .duration(5.0);
 
     let video = slideshow.video(20.0);
     let path = video.make_path();
